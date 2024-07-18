f22feeb4daf67b9c1099bb29f515052467c66c47
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2024-07-18 18:05:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ app.use(cookieParser())
 app.use(cors({
     credentials : true,
     origin : "https://booking-frontend-pi.vercel.app",
-}))
+})) 
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://booking-frontend-pi.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.use('/uploads',express.static(__dirname+'/uploads'))
 
